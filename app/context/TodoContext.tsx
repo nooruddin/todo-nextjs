3d3023d3624ca153bf44bba641d58d862c89da91
Nooruddin Khorasi <nooruddin.khorasi@gmail.com>
2025-02-19 14:30:32 -0800

    adding implementation comments when reducer is not used

--- app/context/TodoContext.tsx
+++ app/context/TodoContext.tsx
@@ -1,11 +1,18 @@
 'use client'
 
 import { createContext, useContext, useReducer, ReactNode } from 'react'
+// For useState implementation:
+// import { createContext, useContext, useState, ReactNode } from 'react'
 import { Todo } from '../todos/page'
+// Remove these imports for useState implementation:
 import { todoReducer, TodoActions, initialState } from '../reducers/todoReducer'
 
 interface TodoContextType {
   todos: Todo[]
+  // For useState, dispatch would be replaced with specific setters:
+  // addTodo: (text: string) => void
+  // toggleTodo: (id: string) => void
+  // deleteTodo: (id: string) => void
   dispatch: React.Dispatch<TodoActions>
 }
 
@@ -17,10 +24,27 @@ interface TodoProviderProps {
 
 export const TodoProvider = ({ children }: TodoProviderProps) => {
   const [todos, dispatch] = useReducer(todoReducer, initialState)
-  // const contextValue = useMemo(() => ({ todos, dispatch }), [todos, dispatch])
+  // useState implementation would look like:
+  // const [todos, setTodos] = useState<Todo[]>([])
+  //
+  // const addTodo = (text: string) => {
+  //   setTodos(prev => [...prev, { id: Date.now().toString(), text, completed: false }])
+  // }
+  //
+  // const toggleTodo = (id: string) => {
+  //   setTodos(prev => prev.map(todo =>
+  //     todo.id === id ? { ...todo, completed: !todo.completed } : todo
+  //   ))
+  // }
+  //
+  // const deleteTodo = (id: string) => {
+  //   setTodos(prev => prev.filter(todo => todo.id !== id))
+  // }
+
   return (
-    // <TodoContext.Provider value={contextValue}>
     <TodoContext.Provider value={{ todos, dispatch }}>
+      {/* With useState it would be: */}
+      {/* <TodoContext.Provider value={{ todos, addTodo, toggleTodo, deleteTodo }}> */}
       {children}
     </TodoContext.Provider>
   )
